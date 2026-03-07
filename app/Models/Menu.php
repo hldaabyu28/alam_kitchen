e3a9789c067827e49da793e1e1df720801065673
Hilda Abiyyu Rahmadani <danirahmadani91@gmail.com>
2026-03-07 13:16:10 +0700

    [REF] model menu and category

--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -2,9 +2,37 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Menu extends Model
 {
-    //
+    use SoftDeletes, HasFactory;
+
+    protected $table = 'menus';
+
+    protected $fillable = [
+        'category_id',
+        'name',
+        'slug',
+        'description',
+        'price',
+        'discount_price',
+        'image',
+        'is_available',
+        'is_special',
+        'order',
+        'stock',
+    ];
+
+    protected $casts = [
+        'is_available' => 'boolean',
+        'is_special' => 'boolean',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(MenuCategory::class, 'category_id');
+    }
 }
--- app/Models/MenuCategory.php
+++ app/Models/MenuCategory.php
@@ -5,15 +5,24 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 class MenuCategory extends Model
 {
     use HasFactory, SoftDeletes;
 
-    protected $table = 'menu_categories';
+    protected $table = 'categories';
 
     protected $fillable = [
         'name',
+        'slug',
         'description',
-        'restaurant_id',
+        'image',
+        'is_active',
+        'order',
     ];
+
+    public function menus()
+    {
+        return $this->hasMany(Menu::class, 'category_id');
+    }
 }
